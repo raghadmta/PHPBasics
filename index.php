2de7fdb14e34aa2e3c75ba08913ef79d4af8dab7
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-16 19:08:25 +0300

    added PHP Basics - String and Variables

--- index.php
+++ index.php
@@ -10,9 +10,18 @@
 
     <?php
     $Stringone = "Hey will there";
-    $biography = "\n"."Will that about "."that";
+    $biography = "\n" . "Will that about " . "that";
     $favorite_food = "\n" . "tur" . "duck" . "en";
-        ?>
+    ?>
+
+    <?php
+    $name = "Raghad";
+    $language = "PHP and MYSql";
+
+    echo "\n" . "HEY! My name is" . $name;
+    echo "\n" . "HEY! My name is" . $name. "I am learning". $language;
+
+    ?>
 </body>
 
 </html>
