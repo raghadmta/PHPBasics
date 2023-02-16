e46221899f6eda5343ec43a49543f14e98378926
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-16 18:41:58 +0300

    added PHP Basics - String and Variables

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    echo "Code" . "cademy";
+    echo "\nMy name is:" . " " . "Raghad";
+    echo "\n" . "tur" . "duck" . "en";
+    ?>
+
+</body>
+
+</html>
