4b7b02b41248b8f07c012a96019363c66fcecf06
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-17 09:19:16 +0300

    added PHP Basics - String and Variables

--- index.php
+++ index.php
@@ -19,7 +19,16 @@
     $language = "PHP and MYSql";
 
     echo "\n" . "HEY! My name is" . $name;
-    echo "\n" . "HEY! My name is" . $name. "I am learning". $language;
+    echo "\n" . "HEY! My name is" . $name . "I am learning" . $language;
+    ?>
+
+    <?php
+    $noun = "code";
+    $adjective = "bignner";
+    $verb = "happy";
+
+    echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+    echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
 
     ?>
 </body>
