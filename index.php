eea994fb0e89457608488e15e6b70a3227d9daca
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-17 09:27:47 +0300

    added PHP Basics - String and Variables

--- index.php
+++ index.php
@@ -31,6 +31,22 @@
     echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
 
     ?>
+
+    <?php
+    $movie = "Home alone";
+
+    $old_favorite = $movie;
+
+
+    echo "I'm a fickle person, my favorite movie used to be $movie.";
+
+    $movie = "The Elaphpant";
+
+    echo "\nBut now my favorite is $movie.";
+
+    echo "my old faviorte movie was $old_favorite";
+    ?>
+
 </body>
 
 </html>
