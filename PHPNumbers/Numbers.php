79a4914d29437afc30c0ce2c07d2c3653d2ea12e
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-18 19:43:18 +0300

    added PHP Basics - Numbers

---
+++ PHPNumbers/Numbers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+    $number1 = 55;
+    $number2 = 11.4;
+
+
+    echo $number1;
+    echo "\n";
+    echo $number2;
+
+    ?>
+</body>
+
+</html>
--- StringAndVariables/AssignByReference.php
+++ StringAndVariables/AssignByReference.php
@@ -5,8 +5,10 @@
 
     <?php
     $very_bad_unclear_name = "15 chicken wings";
-    $order = & $very_bad_unclear_name;
+    // =& opertor is to make allies of the same variable
+    $order =& $very_bad_unclear_name;
 
+    // .= to add info into the value
     $order .= " and 3 soda";
 
     echo "\nYour order is: $very_bad_unclear_name.";
