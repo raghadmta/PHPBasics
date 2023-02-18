236d9c271c84d333f40e56957a49721c5d455332
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-18 20:17:12 +0300

    added PHP Basics - Numbers

---
+++ PHPNumbers/MultiplicationAndDevision.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+    $num_languages = 4;
+    $months = 11;
+    $days = $months * 16;
+    $days_per_language = $days / $num_languages;
+
+    echo $days_per_language;
+
+    ?>
+</body>
+
+</html>
