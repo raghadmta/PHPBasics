52ce6dcb6d90cd8ec282211968dd7ccbe342780f
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 13:51:55 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/BuiltinFunctions/StringFunctions.php
@@ -0,0 +1,12 @@
+<?php
+namespace Codecademy;
+
+// print in reverse
+echo strrev(".pu ti peeK .taerg gniod er'uoY");
+
+// make all letters lowercase 
+echo strtolower("SOON, tHiS WILL Look NoRmAL.");
+
+//repeat the text 
+echo str_repeat("\nThere's no place like home.\n", 3);
+?>
