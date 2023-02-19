7b2cd34eae5a92dffad571eec85eefe8986db275
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 13:45:40 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/BuiltinFunctions/Variables.php
@@ -0,0 +1,14 @@
+<?php
+namespace Codecademy;
+
+$first = "Welcome to the magical world of built-in functions.";
+
+$second = 82137012983;
+//  gettype() function will show the type of the variable
+echo gettype($first);
+echo gettype($second);
+
+//  var_dump() function will give detials about the variable
+echo var_dump($first);
+echo var_dump($second);
+?>
