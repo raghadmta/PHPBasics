f10b79883bf1d4017ba89ecbdced35bc6a55696e
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 11:37:56 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/Parameters/PassByReference.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+    $string_one = "you have teeth";
+    $string_two = "toads are nice";
+    $string_three = "brown is my favorite color";
+
+    function convertToQuestion(&$String)
+    {
+        $String = "Do you think " . $String . "?\n";
+        echo $String;
+    }
+    
+    $word = "Hello";
+    convertToQuestion($word);
+    echo $word;
+
+
+    convertToQuestion($string_one);
+    echo $string_one;
+
+    convertToQuestion($string_two);
+    echo $string_two;
+
+    convertToQuestion($string_three);
+    echo $string_three;
+    ?>
+
+</body>
+
+</html>
