5622a5598605a70900cb87b47500e2483e6ba948
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 14:09:41 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/BuiltinFunctions/NumberFunction.php
@@ -0,0 +1,19 @@
+<?php
+namespace Codecademy;
+
+function calculateDistance($num1, $num2)
+{
+    $total = abs($num1 - $num2); // abs() gives the absolute number .
+    return $total;
+}
+calculateDistance(4, -1);
+
+
+function calculateTip($meal_price)
+{
+    $total = round($meal_price * 1.18); // round the number up
+    return $total;
+}
+echo calculateTip(100);
+
+?>
