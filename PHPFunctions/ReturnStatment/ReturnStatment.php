3d3a8648ab9a0cb980ba102a92eb5406c0ddb38a
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 10:42:21 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/ReturnStatment/ReturnStatment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+function printStringReturnNumber(){
+    echo " Will am a String";
+    return 43;
+
+}
+
+$my_num = printStringReturnNumber();
+
+echo $my_num;
+
+    ?>
+</body>
+
+</html>
