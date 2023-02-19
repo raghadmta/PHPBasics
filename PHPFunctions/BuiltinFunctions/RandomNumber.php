ebe6a743dd409f2d78067db7d825d2256595f146
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 14:15:24 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/BuiltinFunctions/RandomNumber.php
@@ -0,0 +1,14 @@
+<?php
+namespace Codecademy;
+
+//  find out what the maximum random number is in this environment.
+$number = getrandmax();
+echo $number;
+
+echo "\n";
+echo rand();
+
+
+echo "\n";
+echo rand(1, 52);
+?>
