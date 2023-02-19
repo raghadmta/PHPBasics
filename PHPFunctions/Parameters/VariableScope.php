cc43264dc04184d36915e80c91a5f41db8a9ecaf
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-19 11:46:15 +0300

    added PHP Basics - Functions

---
+++ PHPFunctions/Parameters/VariableScope.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+
+    $language = "PHP";
+    $topic = "scope";
+
+    function generateLessonName($concept)
+    {
+        global $language;
+        return $language . ": " . $concept;
+    }
+
+    echo generateLessonName($topic);
+    ?>
+
+</body>
+
+</html>
