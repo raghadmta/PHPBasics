9dca9e52529827c6ed29cfc33119de07deae87ef
Raghad <55548241+raghadmta@users.noreply.github.com>
2023-02-20 14:03:13 +0000

    added PHPBasics - Arrays

---
+++ Arrays/ShiftUnshift.php
@@ -0,0 +1,21 @@
+<?php
+namespace Codecademy;
+
+$record_holders = [];
+
+array_unshift($record_holders, "Tim Montgomery", "Maurice Greene", "Donovan Bailey", "Leroy Burrell", "Carl Lewis");
+
+array_shift($record_holders);
+array_unshift($record_holders, "Justin Gatlin", "Asafa Powell");
+
+array_shift($record_holders);
+array_unshift($record_holders, "Usain Bolt");
+
+?>
+
+
+
+
+
+
+
