5212d32280ad56f39a9f797fab1a81b56fdd067e
Raghad <55548241+raghadmta@users.noreply.github.com>
2023-02-20 12:38:48 +0000

    added PHPBasics - Arrays

---
+++ Arrays/PushPop.php
@@ -0,0 +1,13 @@
+<?php
+namespace Codecademy;
+
+$stack = ["wild success", "failure", "struggle"];
+
+$addd = array_push($stack, "blockr","impedimnt");
+
+array_pop($stack);
+array_pop($stack);
+array_pop($stack);
+array_pop($stack);
+
+?>
