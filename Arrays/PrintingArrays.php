c42ec4ccd7393953195deda9931cc0a7d96d7f63
Raghad <55548241+raghadmta@users.noreply.github.com>
2023-02-20 12:01:29 +0000

    added PHPBasics - Arrays

---
+++ Arrays/PrintingArrays.php
@@ -0,0 +1,12 @@
+<?php
+namespace Codecademy;
+
+$message = ["Oh hey", " You're doing great", " Keep up the good work!\n"];
+
+$favorite_nums = [7, 201, 33, 88, 91];
+
+// implode() function 
+echo implode("!", $message);
+
+// print_r() function
+echo print_r($favorite_nums);
