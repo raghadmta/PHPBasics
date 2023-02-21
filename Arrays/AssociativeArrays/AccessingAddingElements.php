f115c30b27fb6c1096fa937ebfae438689f5e54b
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-21 22:17:42 +0300

    clean format  - associative arrays

---
+++ Arrays/AssociativeArrays/AccessingAddingElements.php
@@ -0,0 +1,21 @@
+<?php
+namespace Codecademy;
+
+$assignment_one = ["Alex" => 87, "Kenny" => 91, "Natalia" => 91, "Lily" => 67, "Dan" => 81, "Kat" => 77, "Sara" => 65];
+
+$assignment_two = ["Alex" => 91, "Kenny" => 99, "Natalia" => 100, "Lily" => 61, "Dan" => 88, "Kat" => 90];
+
+$assignment_three = ["Alex" => 78, "Kenny" => 92, "Natalia" => 94, "Lily" => 79, "Dan" => 73, "Sara" => 61];
+
+$key = "Dan";
+$dans_grades = [$assignment_one["Dan"], $assignment_two["Dan"], $assignment_three["Dan"]];
+
+$student_name = "Alex";
+echo $assignment_two[$student_name];
+
+$assignment_two["Sara"] = 65;
+$assignment_three["Kat"] = 97;
+
+
+
+?>
