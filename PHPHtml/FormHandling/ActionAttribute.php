12c26d7ca7b54d7d48ec51a7dad92100419b2b15
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-22 20:40:22 +0300

    added PHP Basics - PHPHtml

---
+++ PHPHtml/FormHandling/ActionAttribute.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+    <form method="get" action="greet_user.php">
+        First Name:
+        <input type="text" name="first">
+        <br>
+        Last Name:
+        <input type="text" name="last">
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+
+    <a href="index.php">Reset</a>
+</body>
+
+</html>
