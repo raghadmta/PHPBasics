e48f3d9a8b34c3d576b53911f8282447d1d34c7b
Raghad <55548241+raghadmta@users.noreply.github.com>
2023-03-11 00:54:22 +0300

    added PHP Basics - Class

---
+++ Classes/SetGet.php
@@ -0,0 +1,23 @@
+<?php
+class Beverage
+{
+    private $color;
+
+    function setColor($color)
+    {
+        $this->color = strtolower($color);
+
+    }
+    function getColor()
+    {
+        return $this->color;
+    }
+}
+
+$soda = new Beverage();
+
+
+
+
+
+?>
